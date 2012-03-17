31a3431fe76236217fa019a36c22f7edfcb15465
Matias Valdenegro T <matias.valdenegro@gmail.com>
2012-03-16 20:06:06 -0400

    Model for Journal entity.

---
+++ src/library/model/JournalModel.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2011-2012 Matias Valdenegro <matias.valdenegro@gmail.com>
+ * This file is part of KResearch.
+ *
+ * kesearch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * kresearch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Epic Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "JournalModel.h"
+
+#include <QDebug>
+#include <QVariant>
+#include <library/dao/PublicationDAO.h>
+
+JournalModel::JournalModel(PublicationDAO *pubDAO)
+ : QAbstractItemModel(), mPublicationDAO(pubDAO)
+{
+}
+
+JournalModel::~JournalModel()
+{
+}
+
+int JournalModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent)
+
+    return 2;
+}
+
+int JournalModel::rowCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent)
+
+    int ret  = mPublicationDAO->journals().length();
+
+    qDebug() << "How many journals?" << ret;
+
+    return ret;
+}
+QVariant JournalModel::data(const QModelIndex& index, int role) const
+{
+    if(!index.isValid()) {
+        return QVariant();
+    }
+
+    int i = index.row();
+    int j = index.column();
+
+    if(i < 0 || i > mPublicationDAO->journals().count()) {
+        return QVariant();
+    }
+
+    if(role == Qt::DisplayRole) {
+        return handleRead(i, j);
+    }
+
+    return QVariant();
+}
+
+QVariant JournalModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if(orientation != Qt::Horizontal) {
+        return QVariant();
+    }
+
+    if(role != Qt::DisplayRole) {
+        return QVariant();
+    }
+
+    return handleColumnNames(section);
+}
+
+QModelIndex JournalModel::index(int row, int column, const QModelIndex& parent) const
+{
+    Q_UNUSED(parent)
+
+    //TODO: Use the internalId to do indexing into the DAO.
+    return createIndex(row, column, (void *) 0);
+}
+
+QModelIndex JournalModel::parent(const QModelIndex& index) const
+{
+    Q_UNUSED(index);
+
+    return QModelIndex();
+}
+
+Qt::ItemFlags JournalModel::flags(const QModelIndex &index) const
+{
+    Q_UNUSED(index)
+
+    return Qt::ItemIsEnabled;
+}
+
+QVariant JournalModel::handleColumnNames(int column) const
+{
+    switch(column) {
+        case 0: {
+            return "Journal name";
+        }
+
+        case 1: {
+            return "Publication count";
+        }
+    }
+
+    return QVariant();
+}
+
+QVariant JournalModel::handleRead(int row, int column) const
+{
+    switch(column) {
+        case 0: {
+            QString ret = mPublicationDAO->journals().at(row);
+
+            qDebug() << "Returning" << ret;
+
+            return ret;
+        }
+
+        case 1: {
+            return 10;
+        }
+    }
+
+    return QVariant();
+}
---
+++ src/library/model/JournalModel.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2011-2012 Matias Valdenegro <matias.valdenegro@gmail.com>
+ * This file is part of KResearch.
+ *
+ * kesearch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * kresearch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Epic Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef JOURNALMODEL_H_
+#define JOURNALMODEL_H_
+
+#include <QAbstractItemModel>
+
+class PublicationDAO;
+
+class JournalModel : public QAbstractItemModel
+{
+    Q_OBJECT
+
+    public:
+        JournalModel(PublicationDAO *pubDAO);
+        virtual ~JournalModel();
+
+        virtual int columnCount(const QModelIndex& parent = QModelIndex()) const;
+        virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
+        virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+
+        QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
+        virtual QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const;
+        virtual QModelIndex parent(const QModelIndex& index) const;
+
+        Qt::ItemFlags flags(const QModelIndex &index) const;
+
+    private:
+
+        QVariant handleColumnNames(int column) const;
+        QVariant handleRead(int row, int column) const;
+
+        PublicationDAO *mPublicationDAO;
+};
+
+#endif /* JOURNALMODEL_H_ */

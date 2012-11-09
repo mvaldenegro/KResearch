271e72586a1001d46810e50be707b49d40155560
Matias Valdenegro T <matias.valdenegro@gmail.com>
2012-11-09 22:20:21 +0100

    Generic DAO interface.

---
+++ src/library/dao/GenericDAO.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2011-2012 Matias Valdenegro <matias.valdenegro@gmail.com>
+ * This file is part of KResearch.
+ *
+ * kesearch is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * kresearch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KResearch.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GENERICDAO_H_
+#define GENERICDAO_H_
+
+#include <qglobal.h>
+
+template<typename T>
+class GenericDAO
+{
+    public:
+
+        GenericDAO()
+        {
+        }
+
+        virtual ~GenericDAO()
+        {
+        }
+
+        virtual typename T::Ptr findById(qulonglong id) const = 0;
+
+        virtual bool saveOrUpdate(typename T::Ptr entity) = 0;
+
+        virtual typename T::List findAll() const = 0;
+};
+
+
+#endif /* GENERICDAO_H_ */
